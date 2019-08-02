0cb780c2593d6fec57ef5ea02c93689a82597a2f
Niall Gallagher <gallagher_niall@yahoo.com>
2019-08-02 23:44:52 +0100

    Test POST methods

--- simple-module-resource/src/test/java/org/simpleframework/module/resource/container/ServerTest.java
+++ simple-module-resource/src/test/java/org/simpleframework/module/resource/container/ServerTest.java
@@ -16,7 +16,10 @@ import org.simpleframework.module.annotation.Provides;
 import org.simpleframework.module.annotation.Value;
 import org.simpleframework.module.resource.action.JsonMapper;
 import org.simpleframework.module.resource.action.ResponseEntity;
+import org.simpleframework.module.resource.annotation.Body;
+import org.simpleframework.module.resource.annotation.Consumes;
 import org.simpleframework.module.resource.annotation.GET;
+import org.simpleframework.module.resource.annotation.POST;
 import org.simpleframework.module.resource.annotation.Path;
 import org.simpleframework.module.resource.annotation.PathParam;
 import org.simpleframework.module.resource.annotation.Produces;
@@ -94,6 +97,80 @@ public class ServerTest extends TestCase {
       }
    }
    
+   public void testServerPostWithDefaultContentType() throws Exception {
+      Acceptor acceptor = Application.create(ServerDriver.class)
+         .path("..")
+         .module(ServerTest.class)
+         .create("--message=key", "--text=val")
+         .name("Apache/2.2.14")
+         .session("SESSIONID")
+         .threads(10)
+         .start();
+      
+      try {
+         InetSocketAddress address = acceptor.bind();
+         int port = address.getPort();
+         URL target = new URL("http://localhost:" + port + "/some/123456789/value");
+         HttpURLConnection connection = (HttpURLConnection)target.openConnection();
+         
+         connection.setRequestMethod("POST");
+         connection.setDoOutput(true);
+         connection.setRequestProperty("Accept", "*");
+         connection.setRequestProperty("Content-Type", "application/vnd.test-v1+json");
+         connection.getOutputStream().write("{\"id\": \"blah\"}".getBytes());
+         
+         JsonMapper mapper = new JsonMapper();
+         InputStream stream = connection.getInputStream();
+         Map map = mapper.readValue(stream, Map.class);
+         
+         assertEquals(connection.getResponseCode(), 200);
+         assertTrue(connection.getHeaderField("Set-Cookie").contains("TEST=123"));
+         assertEquals(connection.getHeaderField("Content-Type"), "application/json");
+         assertEquals(map.get("id"), "blah");
+         assertEquals(map.size(), 1);
+      } finally {
+         acceptor.close();
+      }
+   }
+   
+   public void testServerPostWithVendorContentType() throws Exception {
+      Acceptor acceptor = Application.create(ServerDriver.class)
+         .path("..")
+         .module(ServerTest.class)
+         .create()
+         .name("Apache/2.2.14")
+         .threads(10)
+         .start();
+      
+      try {
+         InetSocketAddress address = acceptor.bind();
+         int port = address.getPort();
+         URL target = new URL("http://localhost:" + port + "/some/999/value");
+         HttpURLConnection connection = (HttpURLConnection)target.openConnection();
+         
+         connection.setRequestMethod("POST");
+         connection.setDoOutput(true);
+         connection.setRequestProperty("Accept", "application/vnd.test-v1+json");
+         connection.setRequestProperty("Content-Type", "application/vnd.test-v1+json");
+         connection.getOutputStream().write("{\"a\": \"b\", \"c\": \"d\"}".getBytes());
+         
+         JsonMapper mapper = new JsonMapper();
+         InputStream stream = connection.getInputStream();
+         Map map = mapper.readValue(stream, Map.class);
+         
+         assertEquals(connection.getResponseCode(), 200);
+         assertTrue(connection.getHeaderField("Set-Cookie").contains("TEST=999"));
+         assertEquals(connection.getHeaderField("Server"), "Apache/2.2.14");
+         assertEquals(connection.getHeaderField("Content-Type"), "application/vnd.test-v1+json");
+         assertEquals(map.get("a"), "b");
+         assertEquals(map.get("c"), "d");
+         assertEquals(map.size(), 2);
+      } finally {
+         acceptor.close();
+      }
+   }
+   
+   
    @Provides
    public ExampleService service(ExampleComponent component, @Value("${message}") String message) {
       return new ExampleService(component, message);
@@ -136,6 +213,17 @@ public class ServerTest extends TestCase {
          return ResponseEntity.create(Status.OK)
             .cookie("TEST", "123")
             .entity(Collections.singletonMap(service.message, service.component.text))
+            .create();
+      }
+      
+      @POST
+      @Path("/value")
+      @Consumes({"application/json", "application/vnd.test-v1+json"})
+      @Produces({"application/json", "application/vnd.test-v1+json"})
+      public ResponseEntity value(@PathParam("id") String id, @Body String body) {
+         return ResponseEntity.create(Status.OK)
+            .cookie("TEST", id)
+            .entity(body)
             .create();
       }
    }
