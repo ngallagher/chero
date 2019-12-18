cfed89908576df83301aae9af4a4a86af47b729d
Niall Gallagher <gallagher_niall@yahoo.com>
2019-12-18 21:50:15 +0000

    Make cache copy on write

--- simple-resource/src/main/java/org/simpleframework/resource/SubscriptionRouter.java
+++ simple-resource/src/main/java/org/simpleframework/resource/SubscriptionRouter.java
@@ -1,5 +1,6 @@
 package org.simpleframework.resource;
 
+import static java.lang.Boolean.TRUE;
 import static org.simpleframework.http.Protocol.UPGRADE;
 import static org.simpleframework.http.Protocol.WEBSOCKET;
 
@@ -12,17 +13,24 @@ import org.simpleframework.http.socket.service.DirectRouter;
 import org.simpleframework.http.socket.service.Router;
 import org.simpleframework.http.socket.service.Service;
 import org.simpleframework.module.common.Cache;
+import org.simpleframework.module.common.CopyOnWriteCache;
 import org.simpleframework.module.common.LeastRecentlyUsedCache;
 import org.simpleframework.module.core.ComponentManager;
 import org.simpleframework.resource.annotation.Subscribe;
 
 public class SubscriptionRouter implements Router {
    
+   private final Cache<String, Boolean> failures;
    private final Cache<String, Router> routes;
    private final ComponentManager manager;
    
    public SubscriptionRouter(ComponentManager manager) {
-      this.routes = new LeastRecentlyUsedCache<String, Router>();
+      this(manager, 1000);
+   }
+   
+   public SubscriptionRouter(ComponentManager manager, int capacity) {
+      this.failures = new LeastRecentlyUsedCache<String, Boolean>();
+      this.routes = new CopyOnWriteCache<String, Router>();
       this.manager = manager;
    }
 
@@ -34,31 +42,42 @@ public class SubscriptionRouter implements Router {
          if(token.equalsIgnoreCase(WEBSOCKET)) {
             Path path = request.getPath();
             String normal = path.getPath();
-            Router router = routes.fetch(normal);
             
-            if(router == null) {
-               List<Service> services = manager.resolveAll(Service.class);
-               
-               for(Service service : services) {
-                  Class<?> type = service.getClass();
-                  Subscribe label = type.getAnnotation(Subscribe.class);
-                  
-                  if(label != null) {
-                     String pattern = label.value();
-                     
-                     if(normal.matches(pattern)) {
-                        router = new DirectRouter(service);
-                        routes.cache(normal, router);
-                        return router.route(request, response);
-                     }
-                  }
+            if(!failures.contains(normal)) {
+               Router router = match(normal);
+              
+               if(router != null) {
+                  return router.route(request, response);
                }
-               return null;
+               failures.cache(normal, TRUE);
             }
-            return router.route(request, response);
          }
       }
       return null;
+   }
+
+   private Router match(String normal) {
+      Router router = routes.fetch(normal);
+      
+      if(router == null) {
+         List<Service> services = manager.resolveAll(Service.class);
+         
+         for(Service service : services) {
+            Class<?> type = service.getClass();
+            Subscribe label = type.getAnnotation(Subscribe.class);
+            
+            if(label != null) {
+               String pattern = label.value();
+               
+               if(normal.matches(pattern)) {
+                  router = new DirectRouter(service);
+                  routes.cache(normal, router);
+                  return router;
+               }
+            }
+         }
+      }
+      return router;
    }
 
 
